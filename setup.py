a4fd31c2f8de31286d10bc26ba0acd546a07b6fc
Samrat Sharma <samratsharma979@gmail.com>
2025-10-26 11:41:52 +0530

    Setup, Requirments, pyproject.toml

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Samrat Raj Sharma",
+    author_email="Samratsharma979@gmail.com",
+    packages=find_packages()
+) 
